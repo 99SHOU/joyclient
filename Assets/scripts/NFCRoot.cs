9d44422cdc79bdd656f1f19ac04b1b8edc8a5d14
99wyc <865902510@qq.com>
2018-01-15 10:46:37 +0800

    a

---
+++ Assets/scripts/NFCRoot.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using NFSDK;
+using System.IO;
+using ProtoBuf;
+using System;
+
+public class NFCRoot : MonoBehaviour
+{
+    private readonly string m_Accoount = "hello";
+    private readonly string m_LoginIp = "127.0.0.1";
+    private readonly ushort m_LoginPort = 23000;
+
+    private static NFCRoot _instance = null;
+    public static NFCRoot Instance()
+    {
+        return _instance;
+    }
+    void Start()
+    {
+        _instance = this;
+
+        Debug.Log("Root Start");
+
+        DontDestroyOnLoad(gameObject);
+
+        ARCSNetLogic.Instance().OnStart();
+        ARCSNetLogic.Instance().ConnectServer(m_LoginIp, m_LoginPort);
+
+
+        ARCSNetLogic.Instance().AddReceiveCallBack(Msg.EGameMsgID.EgmiLoginRespon, LoginResponHandler);
+        ARCSNetLogic.Instance().AddReceiveCallBack(Msg.EGameMsgID.EgmiConnectToGateRespon, ConnectToGateResponHandler);
+
+        Login();
+    }
+	
+    void OnDestroy()
+    {
+        ARCSNetLogic.Instance().OnDestroy();
+    }
+	
+	void Update () 
+    {
+        ARCSNetLogic.Instance().OnUpdate();
+    }
+
+    void Login()
+    {
+        Msg.LoginReq xData = new Msg.LoginReq();
+        xData.Account = m_Accoount;
+        ARCSNetLogic.Instance().Send<Msg.LoginReq>(Msg.EGameMsgID.EgmiLoginReq, xData);
+    }
+
+    void LoginResponHandler(UInt16 id, MemoryStream stream)
+    {
+        Msg.LoginRespon xData = new Msg.LoginRespon();
+        xData = Serializer.Deserialize<Msg.LoginRespon>(stream);
+
+        if (xData.ResponCode == Msg.LoginResponCode.LoginSuccess)
+        {
+            Debug.Log(xData.Account + xData.GateAddr + xData.Token + xData.ResponCode.ToString());
+            ConnectToGate(m_Accoount, xData.GateAddr, xData.Token);
+        }
+    }
+
+    void ConnectToGate(string account, string gateAddr, string token)
+    {
+        string[] gateInfo = gateAddr.Split(':');
+        ARCSNetLogic.Instance().ConnectServer(gateInfo[0], ushort.Parse(gateInfo[1]));
+
+        Msg.ConnectToGateReq xData = new Msg.ConnectToGateReq();
+        xData.Account = account;
+        xData.Token = token;
+        ARCSNetLogic.Instance().Send<Msg.ConnectToGateReq>(Msg.EGameMsgID.EgmiConnectToGateReq, xData);
+    }
+
+    void ConnectToGateResponHandler(UInt16 id, MemoryStream stream)
+    {
+        Msg.ConnectToGateRespon xData = new Msg.ConnectToGateRespon();
+        xData = Serializer.Deserialize<Msg.ConnectToGateRespon>(stream);
+
+        Debug.Log(xData.Account + xData.ResponCode.ToString());
+    }
+}
---
+++ Assets/scripts/Net/ArcsNetLogic.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using ProtoBuf;
+using UnityEngine;
+
+namespace NFSDK
+{
+    public class ARCSNetLogic
+    {
+        private static ARCSNetLogic _instance = null;
+        public static ARCSNetLogic Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ARCSNetLogic();
+            }
+            return _instance;
+        }
+
+        public ARCSNetLogic()
+        {
+            _instance = this;
+        }
+
+        public void OnStart()
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+            NFCNet.Instance().doUpdate();
+        }
+
+        public void OnDestroy()
+        {
+
+        }
+
+        public void ConnectServer(string ip, UInt16 port)
+        {
+            if (NFCNet.Instance().isConnected())
+                NFCNet.Instance().shutDown();
+            
+            NFCNet.Instance().ready(ip, port);
+            NFCNet.Instance().connect();
+        }
+
+
+
+        {
+            NFCNetDispatcher.Instance().AddReceiveCallBack((UInt16)id, netHandler);
+        }
+        
+        public void Send<T>(Msg.EGameMsgID unMsgID, T data)
+        {
+            MemoryStream stream = new MemoryStream();
+            Serializer.Serialize<T>(stream, data);
+
+            MemoryStream pack = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(pack);
+            UInt32 msgLen = (UInt32)stream.Length + ConstDefine.NF_PACKET_LEN_MSG_ID;
+            writer.Write(NFCNet.ConvertUint32((UInt32)msgLen));
+            writer.Write(NFCNet.ConvertUint16((UInt16)unMsgID));
+            stream.WriteTo(pack);
+            NFCNet.Instance().sendMsg(pack);
+        }
+
+    }
+}
